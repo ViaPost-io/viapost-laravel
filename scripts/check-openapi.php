a62bcdf8f4dc66b38988058728fd0c832150e530
Valdinei F. Lima <valdinei.lima@yatta.net.br>
2026-09-21 17:06:48 -0300

    chore: synchronize Laravel SDK contract snapshot
    (cherry picked from commit e7784db6653726613fc8f91657a2993cddcfa958)

--- scripts/check-openapi.php
+++ scripts/check-openapi.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Symfony\Component\Yaml\Exception\ParseException;
 use Symfony\Component\Yaml\Yaml;
 
-const SNAPSHOT_SHA256 = 'c5d5ae1d85e61b4e14e09351b14146465ce357075d2ed5fe4e034f6ff6693dc1';
+const SNAPSHOT_SHA256 = '4296cf369c8a2b1e27f215fddc36dbafb4203aa35c509095df1048243b8da847';
 
 $autoload = dirname(__DIR__).'/vendor/autoload.php';
 if (! is_file($autoload)) {
